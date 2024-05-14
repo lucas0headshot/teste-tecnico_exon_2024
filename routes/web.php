bbcf53be85b363dfae904c4d3c9a2411ddac2bfb
lucas0headshot <lucas0headshot@gmail.com>
2024-05-14 02:04:54 -0300

    refactor: upd routes
    Remover rota principal.
    Adicionar rotas de recurso pra Consultor & Compromisso.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CompromissoController;
+use App\Http\Controllers\ConsultorController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('consultores', ConsultorController::class);
+
+Route::resource('compromissos', CompromissoController::class);
