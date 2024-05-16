63bb9ba616768379e1b0d751d5251bfcdfe2db62
lucas0headshot <lucas0headshot@gmail.com>
2024-05-16 03:19:53 -0300

    refactor: add import SoftDeletes models
    Adicionar SoftDeletes às Models Consultor & Compromisso.

--- app/Models/Compromisso.php
+++ app/Models/Compromisso.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Compromisso extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['id_consultor', 'data', 'hora_inicio', 'hora_fim', 'intervalo'];
 }
