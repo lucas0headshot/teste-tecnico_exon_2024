6d4b53351de13c3527b831b60c4fb23eb4386ec7
lucas0headshot <lucas0headshot@gmail.com>
2024-05-16 03:21:52 -0300

    refactor: add names às rotas
    Adicionar nome às rotas dos recursos Consultor & Compromisso.

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,22 @@ use App\Http\Controllers\CompromissoController;
 use App\Http\Controllers\ConsultorController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('consultores', ConsultorController::class);
+Route::resource('consultores', ConsultorController::class)->names([
+    'index' => 'consultores.index',
+    'create' => 'consultores.create',
+    'store' => 'consultores.store',
+    'show' => 'consultores.show',
+    'edit' => 'consultores.edit',
+    'update' => 'consultores.update',
+    'destroy' => 'consultores.destroy'
+]);
 
-Route::resource('compromissos', CompromissoController::class);
+Route::resource('compromissos', CompromissoController::class)->names([
+    'index' => 'compromissos.index',
+    'create' => 'compromissos.create',
+    'store' => 'compromissos.store',
+    'show' => 'compromissos.show',
+    'edit' => 'compromissos.edit',
+    'update' => 'compromissos.update',
+    'destroy' => 'compromissos.destroy'
+]);
