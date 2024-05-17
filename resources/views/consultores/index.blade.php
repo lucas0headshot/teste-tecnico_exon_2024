94bcff66452f962c67ea16a98fb16285fbd4ca36
lucas0headshot <lucas0headshot@gmail.com>
2024-05-17 16:39:15 -0300

    refactor: View index
    Adicionar DataTable e processamento serverSide.
    
    Importar a View create_edit.

--- resources/views/consultores/index.blade.php
+++ resources/views/consultores/index.blade.php
@@ -1,13 +1,52 @@
 @extends('layouts.main')
 
-@section('title', 'Exon Sistemas & Consultoria - Consultores')
+@section('title', 'Consultores')
 
 @section('content')
+    @include('consultores.create_edit')
+
     <div>
-        <ul>
-            @foreach($consultores as $consultor)
-                <li>{{ $consultor->nome }}</li>
-            @endforeach
-        </ul>
+        <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#consultorModal">
+            Cadastrar
+        </button>
+
+        <table id="consultores-list" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Valor Hora</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
     </div>
+
+    <script type="module">
+        $(document).ready(function() {
+            const table = $('#consultores-list').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('consultores.index') }}",
+                columns: [
+                    {data: 'nome', name: 'nome'},
+                    {data: 'valor_hora', name: 'valor_hora'},
+                    {
+                        data: 'acao',
+                        name: 'acao',
+                        orderable: true,
+                        searchable: true
+                    }
+                ],
+
+                responsive: true,
+                colReorder: true,
+
+                language: {
+                    url: '/json/DataTables/pt-BR.json',
+                },
+            });
+        });
+    </script>
 @endsection
