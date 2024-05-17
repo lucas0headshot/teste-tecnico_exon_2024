1096151b6b808ca6b46aad05d2451ccf0da67c4e
lucas0headshot <lucas0headshot@gmail.com>
2024-05-17 16:03:11 -0300

    chore: DataTables BS5, ColReorder, Responsive & SearchBuilder
    Adicionar dependências e importação de DataTables BS5, ColReorder, Responsive & SearchBuilder

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,8 @@ import $ from "jquery";
 window.$ = $;
 
 import "bootstrap";
-import "datatables.net-dt";
+
+import "datatables.net-bs5";
+import "datatables.net-colreorder-bs5";
+import "datatables.net-responsive-bs5";
+import "datatables.net-searchbuilder-bs5";
