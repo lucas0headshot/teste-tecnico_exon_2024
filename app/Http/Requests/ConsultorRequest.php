35278750dba544ed243f49031004b3a9cc172320
lucas0headshot <lucas0headshot@gmail.com>
2024-05-18 04:44:15 -0300

    feat: ConsultorRequest
    Adicionar ConsultorRequest.
    Classe cuja retorna as validações para uma requisição, envolvendo as actions Create e Update de um Consultor.

---
+++ app/Http/Requests/ConsultorRequest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+/**
+ *  Cuida da validação da FormRequest de um Consultor.
+ *  Pode ser reutilizada nas actions create e update.
+ */
+class ConsultorRequest extends FormRequest
+{
+    /**
+     * Determina se a request está autorizada.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Retorna as regras de validação;
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        $id_consultor = $this->route('consultor');
+
+        return [
+            'nome' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('consultores')->ignore($id_consultor),
+            ],
+            'valor_hora' => 'required|numeric|min:0'
+        ];
+    }
+
+    /**
+     * Retorna as mensagens de validação.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'nome.required' => 'Informe o nome.',
+            'nome.string' => 'Informe um nome válido.',
+            'nome.max' => 'Informe um nome até 255 caracteres.',
+            'nome.unique' => 'Nome existente.',
+            'valor_hora.required' => 'Informe o valor por hora.',
+            'valor_hora.min' => 'Informe um valor válido.',
+            'valor_hora.numeric' => 'Informe um valor válido.'
+        ];
+    }
+}
