4871024061cfd1945fbf7bfea893c988532bc4c6
lucas0headshot <lucas0headshot@gmail.com>
2024-05-18 09:20:49 -0300

    feat: Model Consultor
    Adicionar método allOnlyIDAndName()

--- app/Models/Consultor.php
+++ app/Models/Consultor.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -15,8 +16,23 @@ class Consultor extends Model
     protected $table = 'consultores';
 
 
+    /**
+     * Retorna se o Consultor está vínculado a um compromisso.
+     *
+     * @return bool
+     */
     public function hasCompromisso(): bool
     {
         return Compromisso::select('id')->where('id_consultor', $this->id)->exists();
+    }
+
+    /**
+     * Retorna Consultores, mas apenas o ID e o nome deles.
+     *
+     * @return Collection
+     */
+    public static function allOnlyIDAndName(): Collection
+    {
+        return Consultor::all(['id', 'nome']);
     }
 }
