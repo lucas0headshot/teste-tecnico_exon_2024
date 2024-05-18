6484173d4506163910292bf83cb00c52112a197d
lucas0headshot <lucas0headshot@gmail.com>
2024-05-18 06:45:21 -0300

    refactor: index Compromisso
    Adicionar lógica p/ carregar table c/ DataTables e AJAX.
    
    Adicionar verificação do retorno "success" de uma das RedirectResponse.

--- resources/views/compromissos/index.blade.php
+++ resources/views/compromissos/index.blade.php
@@ -1,13 +1,73 @@
 @extends('layouts.main')
 
-@section('title', 'Exon Sistemas & Consultoria - Compromissos')
+@section('title', 'Compromissos')
 
 @section('content')
-    <div>
-        <ul>
-            @foreach($compromissos as $compromisso)
-                <li>{{ $compromisso->data }}</li>
-            @endforeach
-        </ul>
+    <h1>Compromissos</h1>
+
+    <div class="mt-5">
+        <a class="btn btn-success" href="{{ route('compromissos.create') }}">Cadastrar</a>
+
+        @if(session('success'))
+            <div class="alert alert-success mt-3" id="mensagemSucesso">
+                {{ session('success') }}
+            </div>
+
+            <script type="module">
+                setTimeout(() => {
+                    $('#mensagemSucesso').remove();
+                }, 3000);
+            </script>
+        @endif
+
+        <table id="compromissos-list" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Consultor</th>
+                    <th>Horários</th>
+                    <th>Total horas</th>
+                    <th>Valor total</th>
+                    <th>Ação</th>
+                </tr>
+            </thead>
+            <tbody></tbody>
+            <tfoot>
+                <tr>
+                    <td>Total</td>
+                    <td><!-- //TODO: somar tudo --></td>
+                </tr>
+            </tfoot>
+        </table>
     </div>
+
+    <script type="module">
+        $(document).ready(function() {
+            const table = $('#compromissos-list').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('compromissos.index') }}",
+                columns: [
+                    {data: 'data', name: 'data'},
+                    {data: 'consultor', name: 'consultor'},
+                    {data: 'horarios', name: 'horarios'},
+                    {data: 'intervalo', name: 'intervalo'},
+                    {data: 'total_horas', name: 'total_horas'},
+                    {data: 'valor_total', name: 'valor_total'},
+                    {
+                        data: 'acao',
+                        orderable: false,
+                        searchable: false
+                    }
+                ],
+
+                responsive: true,
+                //FIXME: colReorder: true,
+
+                language: {
+                    url: '/json/DataTables/pt-BR.json',
+                },
+            });
+        });
+    </script>
 @endsection
